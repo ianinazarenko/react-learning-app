7aa166f1399bfd2bc09a383298af79d71f21eac4
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-02-15 18:24:53 +0300

    Setting up first components, react basics

--- react-learning-app/src/index.js
+++ react-learning-app/src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// CSS
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const books = [
+    {
+        id: 1,
+        img:
+            "https://images-na.ssl-images-amazon.com/images/I/81EVdWdmOKL._AC_UL200_SR200,200_.jpg",
+        title: "Brown Bear, Brown Bear, What Do You See?",
+        author: "Bill Martin Jr.",
+    },
+    {
+        id: 2,
+        img:
+            "https://images-na.ssl-images-amazon.com/images/I/71l9WWa-rXL._AC_UL200_SR200,200_.jpg",
+        title: "Milk and Honey",
+        author: "Rupi Kaur",
+    },
+];
+
+function BookList() {
+    // This is JSX
+    return (
+        <section className="booklist">
+            {books.map((book) => {
+                return <Book key={book.id} {...book}></Book>;
+            })}
+        </section>
+    );
+}
+
+const Book = (props) => {
+    console.log(props);
+    const { img, title, author } = props;
+    return (
+        <article className="book">
+            <img src={img} alt={title} />
+            <h1>{title}</h1>
+            <h4>{author}</h4>
+        </article>
+    );
+};
+
+ReactDOM.render(<BookList />, document.getElementById("root"));

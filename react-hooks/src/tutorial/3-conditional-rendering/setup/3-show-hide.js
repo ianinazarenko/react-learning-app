82cffc29a58ee2503b8383d65df4882bccb29c5a
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-02-18 21:30:59 +0300

    conditional rendering in progress

--- react-hooks/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ react-hooks/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+    const [show, setShow] = useState(false);
+
+    return (
+        <>
+            <button
+                className="btn"
+                onClick={() => {
+                    setShow(!show);
+                }}
+            >
+                show/hide
+            </button>
+            {show && <Item />}
+        </>
+    );
 };
+
+const Item = () => {
+    return (
+        <div style={{ marginTop: "2rem" }}>
+            <h1>window</h1>
+            <h2>size:</h2>
+        </div>
+    );
+};
+
+class User {
+    constructor(name, job = "admin") {
+        this.name = name;
+        this.job = job;
+    }
+}
+
+class Admin extends User {
+    constructor(name, isAdmin) {
+        super();
+        this.isAdmin = isAdmin;
+        // this.name = name;
+    }
+}
+
+const user1 = new User("Jake", "farmer");
+const admin1 = new Admin("Tom", true);
+
+console.log(user1);
+console.log(admin1);
 
 export default ShowHide;

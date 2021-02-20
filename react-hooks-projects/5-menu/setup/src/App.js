1927cd6109b98f582cca2f546a8be7c28c0ba265
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-02-20 14:01:28 +0300

    5-menu project is finished

--- react-hooks-projects/5-menu/setup/src/App.js
+++ react-hooks-projects/5-menu/setup/src/App.js
@@ -1,10 +1,36 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
-  return <h2>menu project setup</h2>;
+    const [menuItems, setMenuItems] = useState(items);
+    const [categories, setCategories] = useState(allCategories);
+
+    const filterItems = (category) => {
+        if (category === "all") {
+            setMenuItems(items);
+            return;
+        }
+        const filteredItems = items.filter(
+            (item) => item.category === category
+        );
+        setMenuItems(filteredItems);
+    };
+    return (
+        <main>
+            <section className="section menu">
+                <div className="title">
+                    <h2>our menu</h2>
+                    <div className="underline"></div>
+                </div>
+                <Categories filterItems={filterItems} categories={categories} />
+                <Menu items={menuItems} />
+            </section>
+        </main>
+    );
 }
 
 export default App;

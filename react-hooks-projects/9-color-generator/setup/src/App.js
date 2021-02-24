b4689bba588a12b7fdd2d8379869f977628c71f2
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-02-24 16:29:16 +0300

    9-color-generator project is finished

--- react-hooks-projects/9-color-generator/setup/src/App.js
+++ react-hooks-projects/9-color-generator/setup/src/App.js
@@ -1,10 +1,50 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+    const [color, setColor] = useState("");
+    const [error, setError] = useState(false);
+    const [list, setList] = useState(new Values("#f15025").all(10));
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        try {
+            const colors = new Values(color).all(10);
+            setList(colors);
+        } catch (error) {
+            setError(true);
+            console.log(error);
+        }
+    };
+
+    return (
+        <>
+            <section className="container">
+                <h3>Color generator</h3>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        value={color}
+                        placeholder="#f15025"
+                        onChange={(e) => {
+                            setColor(e.target.value);
+                        }}
+                        className={error ? "error" : null}
+                    />
+                    <button className="btn" type="submit">
+                        generate
+                    </button>
+                </form>
+            </section>
+            <section className="colors">
+                {list.map((color, index) => {
+                    return <SingleColor key={index} index={index} {...color} />;
+                })}
+            </section>
+        </>
+    );
 }
 
-export default App
+export default App;
--- react-hooks-projects/9-color-generator/setup/src/SingleColor.js
+++ react-hooks-projects/9-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ index, rgb, type, weight }) => {
+    const [alert, setAlert] = useState(false);
+    const bcg = rgb.join(",");
+    const hex = rgbToHex(...rgb);
 
-export default SingleColor
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (alert) setAlert(false);
+        }, 3000);
+        return () => clearTimeout(timer);
+    }, [alert]);
+
+    const handleClick = () => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex);
+    };
+    return (
+        <article
+            className={`color ${type !== "tint" && "color-light"}`}
+            style={{ backgroundColor: `rgb(${bcg})` }}
+            onClick={handleClick}
+        >
+            <p className="percent-value">{weight}%</p>
+            <p className="color-value">{hex}</p>
+            {alert && <p className="alert">copied to clipboard</p>}
+        </article>
+    );
+};
+
+export default SingleColor;

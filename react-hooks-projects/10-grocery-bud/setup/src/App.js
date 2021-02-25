d9acd1ca48ec1a3238e75e031eba3b0e055c4e45
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-02-25 21:07:28 +0300

    Working on editig of item of 9-grocery-bud

--- react-hooks-projects/10-grocery-bud/setup/src/App.js
+++ react-hooks-projects/10-grocery-bud/setup/src/App.js
@@ -1,9 +1,93 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>
+    const [name, setName] = useState("");
+    const [list, setList] = useState([]);
+    const [isEditing, setIsEditig] = useState(false);
+    const [editID, setEditID] = useState(null);
+    const [alert, setAlert] = useState({
+        show: false,
+        msg: "",
+        type: "",
+    });
+
+    const showAlert = (show = false, type = "", msg = "") => {
+        setAlert({ show, type, msg });
+    };
+
+    const clearList = () => {
+        showAlert(true, "danger", "empty list");
+        setList([]);
+    };
+
+    const removeItem = (id) => {
+        showAlert(true, "danger", "item removed");
+        setList(list.filter((item) => item.id !== id));
+    };
+
+    const editItem = (id) => {
+        const specificItem = list.find((item) => item.id === id);
+        setIsEditig(true);
+        setEditID(id);
+        setName(specificItem.title);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submit");
+        if (!name) {
+            // set alert
+            showAlert(true, "danger", "please enter value");
+        } else if (name && isEditing) {
+            // deal with editing
+        } else {
+            showAlert(true, "success", "item added to the list");
+            const newItem = {
+                id: new Date().getTime().toString(),
+                title: name,
+            };
+            setList([...list, newItem]);
+            setName("");
+        }
+    };
+
+    return (
+        <section className="section-center">
+            <form className="grocery-form" onSubmit={handleSubmit}>
+                {alert.show && (
+                    <Alert {...alert} removeAlert={showAlert} list={list} />
+                )}
+                <div className="form-control">
+                    <input
+                        type="text"
+                        name=""
+                        id=""
+                        className="grocery"
+                        placeholder="e.g. eggs"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <button className="submit-btn">
+                        {isEditing ? "edit" : "submit"}
+                    </button>
+                </div>
+            </form>
+            {list.length > 0 && (
+                <div className="grocery-container">
+                    <List
+                        items={list}
+                        removeItem={removeItem}
+                        editItem={editItem}
+                    />
+                    <button className="clear-btn" onClick={clearList}>
+                        remove item
+                    </button>
+                </div>
+            )}
+        </section>
+    );
 }
 
-export default App
+export default App;
--- react-hooks/src/tutorial/6-useReducer/setup/index.js
+++ react-hooks/src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,28 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
+const reducer = (state, action) => {
+    console.log(state, action);
+    return state;
+};
+
+const initialState = {
+    people: [],
+    isModalOpen: false,
+    modalContent: "",
+};
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+    // const [state, dispatch] = useReducer(reducer, initialState, init);
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    let a = true;
+    if (a) {
+        dispatch({ type: "TESTING" });
+        a = false;
+    }
+    return <h2>useReducer</h2>;
 };
 
 export default Index;

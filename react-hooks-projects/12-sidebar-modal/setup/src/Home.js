b636cd18bd7da7cb46198f1a90e8045c58ac3d42
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-02-26 16:28:58 +0300

    12-sidebar-modal project is finished

--- react-hooks-projects/12-sidebar-modal/setup/src/Home.js
+++ react-hooks-projects/12-sidebar-modal/setup/src/Home.js
@@ -1,8 +1,19 @@
-import React, { useContext } from 'react'
-import { FaBars } from 'react-icons/fa'
+import React, { useContext } from "react";
+import { FaBars } from "react-icons/fa";
+import { AppContext } from "./context";
 
 const Home = () => {
-  return <h2>home component</h2>
-}
+    const { openModal, openSidebar } = useContext(AppContext);
+    return (
+        <main>
+            <button className="sidebar-toggle" onClick={openSidebar}>
+                <FaBars />
+            </button>
+            <button className="btn" onClick={openModal}>
+                show modal
+            </button>
+        </main>
+    );
+};
 
-export default Home
+export default Home;
--- react-hooks-projects/12-sidebar-modal/setup/src/context.js
+++ react-hooks-projects/12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,47 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+function AppProvider(props) {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    };
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    };
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const { children } = props;
+
+    return (
+        <AppContext.Provider
+            value={{
+                isModalOpen,
+                isSidebarOpen,
+                openModal,
+                openSidebar,
+                closeModal,
+                closeSidebar,
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
+}
+
+function useGlobalContext() {
+    return useContext(AppContext);
+}
+
+export { AppContext, AppProvider, useGlobalContext };

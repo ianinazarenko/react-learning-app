b5f16dab550edac84a0d30b52c90ce4d534ec9b7
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-02-26 19:01:24 +0300

    13-stripe-submenu project is finished

--- react-hooks-projects/13-stripe-submenus/setup/src/Navbar.js
+++ react-hooks-projects/13-stripe-submenus/setup/src/Navbar.js
@@ -1,9 +1,55 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
-}
+    const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
 
-export default Navbar
+    const displayMenu = (e) => {
+        const page = e.target.textContent;
+        const tempBtn = e.target.getBoundingClientRect();
+        const center = (tempBtn.left + tempBtn.right) / 2;
+        const bottom = tempBtn.bottom - 3;
+
+        openSubmenu(page, { center, bottom });
+    };
+
+    const handleSubmenu = (e) => {
+        if (!e.target.classList.contains("link-btn")) {
+            closeSubmenu();
+        }
+    };
+    return (
+        <nav className="nav">
+            <div className="nav-center">
+                <div className="nav-header">
+                    <img src={logo} alt="logo" className="stripe" />
+                    <button className="btn toggle-btn" onClick={openSidebar}>
+                        <FaBars />
+                    </button>
+                </div>
+                <ul className="nav-links">
+                    <li>
+                        <button className="link-btn" onMouseOver={displayMenu}>
+                            products
+                        </button>
+                    </li>
+                    <li>
+                        <button className="link-btn" onMouseOver={displayMenu}>
+                            developers
+                        </button>
+                    </li>
+                    <li>
+                        <button className="link-btn" onMouseOver={displayMenu}>
+                            company
+                        </button>
+                    </li>
+                </ul>
+                <button className="btn signin-btn">Sign in</button>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- react-hooks-projects/13-stripe-submenus/setup/src/context.js
+++ react-hooks-projects/13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,54 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+function AppProvider(props) {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+    const [location, setLocation] = useState({});
+    const [page, setPage] = useState({ page: "", links: [] });
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    };
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    };
+
+    const openSubmenu = (text, coordinates) => {
+        const page = sublinks.find((item) => item.page === text);
+        setPage(page);
+        setLocation(coordinates);
+        setIsSubmenuOpen(true);
+    };
+
+    const closeSubmenu = () => {
+        setIsSubmenuOpen(false);
+    };
+
+    const { children } = props;
+    return (
+        <AppContext.Provider
+            value={{
+                isSubmenuOpen,
+                isSidebarOpen,
+                openSubmenu,
+                openSidebar,
+                closeSubmenu,
+                closeSidebar,
+                location,
+                page,
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
+}
+
+function useGlobalContext() {
+    return useContext(AppContext);
+}
+
+export { AppProvider, useGlobalContext };

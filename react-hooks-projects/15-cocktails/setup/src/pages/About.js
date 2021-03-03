363f0cac48927cf89572c71fa143adcb13d2956c
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-03-03 20:11:13 +0300

    15-cocktails project started

--- react-hooks-projects/15-cocktails/setup/src/pages/About.js
+++ react-hooks-projects/15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">About us</h1>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
+				nisi id a beatae explicabo aut amet, debitis exercitationem
+				dolorem iusto, sit ullam ex harum ipsa mollitia quibusdam ipsam
+				inventore? Ab maxime, tenetur esse, modi deserunt iusto corrupti
+				officiis molestiae soluta minus nulla earum.
+			</p>
+		</section>
+	);
+};
 
-export default About
+export default About;

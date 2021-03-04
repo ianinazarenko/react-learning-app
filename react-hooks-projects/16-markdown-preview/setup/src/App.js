14315f98ab1f3a940c451249ce9935d7c3a615c7
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-03-04 17:18:50 +0300

    16-markdown project is finished

--- react-hooks-projects/16-markdown-preview/setup/src/App.js
+++ react-hooks-projects/16-markdown-preview/setup/src/App.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react'
-import ReactMarkdown from 'react-markdown'
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 function App() {
-  return <h2>markdown preview starter</h2>
+	const [markdown, setMarkdown] = useState("## markdown preview");
+
+	return (
+		<main>
+			<section className="markdown">
+				<textarea
+					value={markdown}
+					onChange={(e) => {
+						setMarkdown(e.target.value);
+					}}
+					className="input"
+				></textarea>{" "}
+				<article className="result">
+					<ReactMarkdown>{markdown}</ReactMarkdown>
+				</article>
+			</section>
+		</main>
+	);
 }
 
-export default App
+export default App;

670cdd70acfddbcd86358c2e835d28df246fd2e7
Ianina Nazarenko <ianina.nazarenko@arcadia.spb.ru>
2021-03-05 21:04:23 +0300

    20-dark-mode project is finised

--- react-hooks-projects/20-dark-mode/setup/src/App.js
+++ react-hooks-projects/20-dark-mode/setup/src/App.js
@@ -1,9 +1,49 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from 'react';
+import data from './data';
+import Article from './Article';
+
+const getStorageTheme = () => {
+  const appTheme = localStorage.getItem('theme');
+  if (appTheme) {
+    return appTheme;
+  } else {
+    return 'light-theme';
+  }
+};
 
 function App() {
-  return <h2>dark mode starter</h2>
+  const [theme, setTheme] = useState(getStorageTheme());
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    document.documentElement.className = theme;
+  }, [theme]);
+
+  return (
+    <main>
+      <nav>
+        <div className='nav-center'>
+          <h1>overreacred</h1>
+          <button className='btn' onClick={toggleTheme}>
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className='articles'>
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;

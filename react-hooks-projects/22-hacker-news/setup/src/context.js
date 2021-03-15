b1e6ab83bb8ab0a332bf0eaafa6f9284b161d64f
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-03-15 15:57:59 +0300

    22-hacker-news project is finished

--- react-hooks-projects/22-hacker-news/setup/src/context.js
+++ react-hooks-projects/22-hacker-news/setup/src/context.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useReducer } from 'react'
+import React, { useContext, useEffect, useReducer } from 'react';
 
 import {
   SET_LOADING,
@@ -6,21 +6,72 @@ import {
   REMOVE_STORY,
   HANDLE_PAGE,
   HANDLE_SEARCH,
-} from './actions'
-import reducer from './reducer'
+} from './actions';
+import reducer from './reducer';
 
-const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?';
 
-const initialState = {}
+const initialState = {
+  isLoading: true,
+  hits: [],
+  query: 'react',
+  page: 0,
+  nbPages: 0,
+};
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // Remove story
+  const handleRemoveStory = (id) => {
+    dispatch({ type: REMOVE_STORY, payload: id });
+  };
+
+  // Search
+  const handleSearch = (query) => {
+    dispatch({ type: HANDLE_SEARCH, payload: query });
+  };
+
+  // Page toggle
+  const handlePage = (value) => {
+    dispatch({ type: HANDLE_PAGE, payload: value });
+  };
+
+  // useEffect
+  useEffect(() => {
+    const url = `${API_ENDPOINT}query=${state.query}&page=${state.page}`;
+    const fetchData = async () => {
+      dispatch({ type: SET_LOADING });
+      try {
+        const response = await fetch(url);
+        if (response.ok) {
+          const data = await response.json();
+          dispatch({
+            type: SET_STORIES,
+            payload: { hits: data.hits, nbPages: data.nbPages },
+          });
+          console.log(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [state.query, state.page]);
+
+  return (
+    <AppContext.Provider
+      value={{ ...state, handleRemoveStory, handleSearch, handlePage }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
--- react-hooks-projects/22-hacker-news/setup/src/reducer.js
+++ react-hooks-projects/22-hacker-news/setup/src/reducer.js
@@ -4,7 +4,46 @@ import {
   REMOVE_STORY,
   HANDLE_PAGE,
   HANDLE_SEARCH,
-} from './actions'
+} from './actions';
 
-const reducer = () => {}
-export default reducer
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return { ...state, isLoading: true };
+
+    case SET_STORIES:
+      return {
+        ...state,
+        isLoading: false,
+        hits: action.payload.hits,
+        nbPages: action.payload.nbPages,
+      };
+
+    case REMOVE_STORY:
+      const id = action.payload;
+      const newHits = state.hits.filter((hit) => hit.objectID !== id);
+      return { ...state, hits: newHits };
+
+    case HANDLE_SEARCH:
+      return { ...state, query: action.payload.query, page: 0 };
+
+    case HANDLE_PAGE:
+      if (action.payload === 'dec') {
+        if (state.page > 1) {
+          return { ...state, page: state.page - 1 };
+        } else {
+          return { ...state, page: state.nbPages - 1 };
+        }
+      } else if (action.payload === 'inc') {
+        if (state.page + 1 !== state.nbPages) {
+          return { ...state, page: state.page + 1 };
+        } else {
+          return { ...state, page: 0 };
+        }
+      }
+
+    default:
+      throw new Error(`No matching for ${action.type} action type`);
+  }
+};
+export default reducer;

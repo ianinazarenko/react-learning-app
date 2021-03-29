14b8679fe5ee725a2a81144235d83487c7ad7211
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-03-29 19:17:01 +0300

    Initial set up for products page with grid or list view

--- react-course-comfy-sloth/src/context/filter_context.js
+++ react-course-comfy-sloth/src/context/filter_context.js
@@ -12,7 +12,11 @@ import {
 } from '../actions';
 import { useProductsContext } from './products_context';
 
-const initialState = { filtered_products: [], all_products: [] };
+const initialState = {
+  filtered_products: [],
+  all_products: [],
+  grid_view: true,
+};
 
 const FilterContext = React.createContext();
 
@@ -24,7 +28,7 @@ export const FilterProvider = ({ children }) => {
     dispatch({ type: LOAD_PRODUCTS, payload: products });
   }, [products]);
   return (
-    <FilterContext.Provider value='filter context'>
+    <FilterContext.Provider value={{ ...state }}>
       {children}
     </FilterContext.Provider>
   );
